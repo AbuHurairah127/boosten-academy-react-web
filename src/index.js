1dfd4155fc8c9ae82ea9a08657d6931a6ff63eaf
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-05 22:20:34 +0500

    window.notify global function created

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./config/store";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+    <ToastContainer />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
