a147ebae2789e72eeac9de8b9c76640e275f4453
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-16 22:52:59 +0500

    bottom redisigned removal of newsletter section

--- src/components/bottom/Bottom.js
+++ src/components/bottom/Bottom.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./Bottom.css";
 import { AiOutlineInstagram, AiFillFacebook } from "react-icons/ai";
 import { ImWhatsapp } from "react-icons/im";
-import Button from "../button/Button";
 import { HashLink } from "react-router-hash-link";
 
 const Bottom = () => {
   return (
-    <div className="max-w-screen bg-[#1d3557] text-white flex flex-col justify-around  lg:min-h-[50vh] flex-wrap border-b-2 border-b-[#fff]">
+    <div className="max-w-screen bg-[#1d3557] text-white flex flex-col justify-around  lg:min-h-[30vh] flex-wrap border-b-2 border-b-[#fff]">
       <div className="w-full flex justify-between flex-wrap flex-col items-center lg:flex-row lg:items-start">
-        <div className="left flex flex-col justify-between ml-5 lg:min-h-[35vh] lg:w-1/2">
+        <div className="left flex flex-col justify-between ml-5 lg:min-h-[20vh] lg:w-1/2">
           <div className="leftHeader flex flex-col items-center lg:items-start pt-5 lg:pt-0">
             <HashLink smooth to="/#top">
               <span
@@ -59,22 +58,9 @@ const Bottom = () => {
             </a>
           </div>
         </div>
+
         <div className="right lg:mr-5">
-          <div className="rightHeader flex flex-col  lg:justify-between h-full items-center lg:items-start">
-            <div>
-              <h1 className="text-white pb-5 text-xl">Newsletter :</h1>
-              <div className="flex flex-col lg:flex-row items-center lg:items-start">
-                <input
-                  type="email"
-                  placeholder="Your Email Here"
-                  required
-                  className="md:w-[60vw] lg:w-auto outline-0 px-4 py-1.5 rounded-sm border-2 text-black z-10"
-                />
-                <div className="lg:relative lg:right-2 z-0">
-                  <Button label="Subscribe" />
-                </div>
-              </div>
-            </div>
+          <div className="rightHeader flex flex-col  lg:justify-center h-full items-center lg:items-start">
             <div className="py-5 lg:py-0">
               <p className="text-[#628dc5]">
                 Address:{" "}
